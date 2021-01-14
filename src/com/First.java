c47cfc6cdcea519459842d2e68777e7d4737cb01
TaovOVO <738361079@qq.com>
2021-01-14 14:37:43 +0800

    aaa

---
+++ src/com/First.java
@@ -0,0 +1,25 @@
+package com;
+
+/**
+ * @Description TODO
+ * @Author taov
+ * @Name First
+ * @Date 2021/1/14 14:31
+ */
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "First")
+public class First extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPost");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet");
+    }
+}
